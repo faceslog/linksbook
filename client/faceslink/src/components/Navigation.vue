674807ad2be830132f9838ec456832d881bc3bdc
Faces <66552663+FacesHC@users.noreply.github.com>
2021-02-06 23:18:47 +0100

    nav component

--- client/faceslink/src/components/Navigation.vue
+++ client/faceslink/src/components/Navigation.vue
@@ -1,37 +1,56 @@
 <template>
-    <header class="py-4 px-6 absolute top-0 left-0 right-0 z-10">
-      <div class="container mx-auto text-white">
-        <div class="flex items-center">
-          <div class="flex-1">
-            <!-- Logo <img src="@/assets/img/logo.svg" class="h-6"> -->
-          </div>
-          <button class="text-secondary lg:hidden">
-            <svg class="current-fill h-6 w-6" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="-49 141 512 512" style="enable-background:new -49 141 512 512;" xml:space="preserve">
-              <g>
-                <g>
-                  <path d="M413,422H1c-13.807,0-25-11.193-25-25s11.193-25,25-25h412c13.807,0,25,11.193,25,25S426.807,422,413,422z"/>
-                </g>
-                <g>
-                  <path d="M413,562H1c-13.807,0-25-11.193-25-25s11.193-25,25-25h412c13.807,0,25,11.193,25,25S426.807,562,413,562z"/>
-                </g>
-                <g>
-                  <path d="M413,282H1c-13.807,0-25-11.193-25-25s11.193-25,25-25h412c13.807,0,25,11.193,25,25S426.807,282,413,282z"/>
-                </g>
-              </g>
+    <nav id="header" class="fixed w-full z-30 top-0 text-white bg-black">
+      <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <!-- Logo -->
+        <div class="pl-4 flex items-center">
+          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-3xl" href="/">
+            LINKSBOOK
+          </a>
+        </div>
+        <!-- End Logo -->
+
+        <!-- Menu for Mobile phone-->
+        <div class="block lg:hidden pr-4">
+          <button id="nav-toggle" class="flex items-center p-1 text-blue-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
-          <nav class="hidden lg:flex items-center">
-            <router-link to="/" class="px-6 py-3 font-bold uppercase text-secondary hover:text-primary">Home</router-link>
-            <router-link to="/auth" class="px-6 py-3 font-bold uppercase text-primary">Login</router-link>
-          </nav>
+        </div>
+        <!-- End Menu for Mobile phone-->
+
+
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-black lg:bg-transparent text-white p-4 lg:p-0 z-20" id="nav-content">
+          <ul class="list-reset lg:flex justify-end flex-1 items-center -mr-20">
+            
+            <li class="mr-3">
+              <a class="inline-block py-2 px-4 text-white font-bold no-underline" href="#">About</a>
+            </li>
+
+            <li class="mr-3">
+              <a class="inline-block py-2 px-4 text-white font-bold no-underline" href="#">Contribute</a>
+            </li>
+
+            <li class="">
+              <router-link to="/auth" class="inline-block py-2 px-4 text-white font-bold no-underline" href="#">Sign In</router-link>
+            </li>
+          </ul>
+
         </div>
       </div>
-    </header>
+      <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+    </nav>
 </template>
 
 <script>
 export default {
   name: "Nav",
+  mounted(){
+    document.getElementById('nav-toggle').onclick = function(){
+		document.getElementById("nav-content").classList.toggle("hidden");
+    }
+  }
+
 }
 </script>
